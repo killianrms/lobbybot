4dd415e0b76e4bdf379a03281628b118b5f6c7b6
Killianrms <114222000+killianrms@users.noreply.github.com>
2024-01-17 15:54:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-# setup.py
-
 import setuptools
+
+with open("requirements.txt", "r") as f:
+    install_requires = f.read().splitlines()
 
 setuptools.setup(
     name="oimbot",
-    version="10.5.2",
+    version="10.5.3",
     author="Aeroz",
     long_description="A Fortnite LobbyBot creation tool",
     description="Lobby_Bot_Ftn",
@@ -15,14 +16,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=[
-        'crayons',
-        'BenBotAsync',
-        'fortnitepy @ git+https://github.com/Luc1412/fortnitepy.git',
-        'FortniteAPIAsync',
-        'sanic==21.6.2',
-        'colorama',
-        'aiohttp',
-    ],
+    install_requires=install_requires,
     include_package_data=True,
 )
