ad811dc060295d472fee5fe74e0d199e030f3439
Killianrms <114222000+killianrms@users.noreply.github.com>
2024-01-17 16:25:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
     
 setuptools.setup(
     name="oimbot",
-    version="10.5.2",
+    version="10.5.3",
     author="Aeroz",
     long_description="For Create Fortnite LobbyBot",
     description="Lobby_Bot_Ftn",
